14705c4a1c5c836797ec917b4ace7aa58a371e9c
rapha87 <rafael.santos@CEUMA.EDU.BR>
2019-10-31 11:53:36 -0300

    novo projeto já com algumas correções

---
+++ src/services/mocks/users.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    name: 'Marcus',
+    email: 'marcus.alex@gmail.com',
+    telefone: '21999998888',
+    status: true
+  },
+  {
+    name: 'Maria',
+    email: 'maria.maria@gmail.com',
+    telefone: '21999997777',
+    status: false
+  },
+  {
+    name: 'Alex',
+    email: 'alex.alex@gmail.com',
+    telefone: '21999996666',
+    status: true
+  },
+  {
+    name: 'João',
+    email: 'joao.joao@gmail.com',
+    status: true
+  },
+  {
+    name: 'Valentina',
+    email: 'valentina.valentina@gmail.com',
+    telefone: '21999996666',
+    status: true
+  }
+];
